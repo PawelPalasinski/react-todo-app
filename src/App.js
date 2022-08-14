47d286d90718269a676692c3bad89184b6b1793a
PawelPalasinski <pawel.palasinski@gmail.com>
2022-08-15 00:48:19 +0200

    app.js done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+
+    // add the todo to the list
+    setList([...list, newTodo]);
+
+    // clear input box
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    // Filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
+
   return (
-    <div className="App">
-      Hello world! Działa!
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => addTodo(input)}>Add</button>
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
